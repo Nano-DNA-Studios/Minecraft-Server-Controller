d87c2f62c9afee52628bbc4b11edc1e46014d7c2
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-08-27 21:03:02 -0400

    Added a transfer script
    - Added a script to make transferring and testing easy

---
+++ transfer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Config
+IMAGE_NAME="minecraft-server-controller-app"
+TAG="latest"
+TARGET_COMPOSE_DIR="~/Services/Minecraft-Server"
+
+echo "--- STEP 1: Deep Cleaning Project ---"
+rm -rf Automatic-Bluray-Ripping/bin
+rm -rf Automatic-Bluray-Ripping/obj
+
+echo "--- STEP 2: Publishing for Linux-x64 ---"
+dotnet publish "$PROJECT_FILE" -c Debug -r linux-x64 --self-contained true
+
+echo "--- STEP 3: Building Local Docker Image ---"
+docker build --build-arg BUILD_CONFIG="Debug" -t "${IMAGE_NAME}:${TAG}" .
+
+echo "--- STEP 4: Transferring and Loading Image Directly ---"
+docker save "${IMAGE_NAME}:${TAG}" | ssh rnaserver "docker load"
+
+echo "--- STEP 5: Restarting Docker Compose on Target ---"
+ssh rnaserver << EOF
+  echo "--> Navigating to compose directory..."
+  cd ${TARGET_COMPOSE_DIR}
+  
+  echo "--> Stopping old containers..."
+  docker compose down
+  
+  echo "--> Starting new containers..."
+  docker compose up -d --force-recreate
+  
+  echo "--> Done!"
+EOF
+
+echo "--- LOOP COMPLETE ---"
+
+
